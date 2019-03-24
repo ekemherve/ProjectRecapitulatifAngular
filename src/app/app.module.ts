f572cbee3765c185f880be90078dc8d694b202a3
Romuald Ekem <RomualdHerve.EkemKingni@busi.eu>
2019-03-24 15:24:21 +0100

    Add welcomeComponent and add routeroutlet for the routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AuthModule} from './module/auth/auth.module';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import {Route, RouterModule} from '@angular/router';
+import { WelcomeComponent } from './component/welcome/welcome.component';
+
+const routes: Route[] = [
+  {path: 'welcome', component: WelcomeComponent},
+  {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+  {path: '**', 'component': WelcomeComponent}];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AuthModule
+    AuthModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
