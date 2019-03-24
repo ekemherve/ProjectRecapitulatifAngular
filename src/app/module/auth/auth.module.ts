a03d3cdb7bd230bc8e0a91aec65a394ca6294a64
Romuald Ekem <RomualdHerve.EkemKingni@busi.eu>
2019-03-24 15:12:46 +0100

    Add AngularMaterialModule, the required imports and import AngularMaterialModule in appModule

--- src/app/module/auth/auth.module.ts
+++ src/app/module/auth/auth.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignupComponent } from './component/signup/signup.component';
+import {Route, RouterModule} from '@angular/router';
+import {AngularMaterialModule} from '../angular-material/angular-material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+const  routes: Route[] = [
+  {path: 'register', component: SignupComponent},
+];
 
 @NgModule({
   declarations: [SignupComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AngularMaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class AuthModule { }
